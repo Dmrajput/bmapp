638f1b7ee9601c0ffac4535f148e68b327ed3beb
Divyaraj <divyarajsinh5216@gmail.com>
2026-01-18 16:34:22 +0530

    config

---
+++ backend/src/config/s3.js
@@ -0,0 +1,20 @@
+// import AWS from "aws-sdk";
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_KEY,
+//   region: process.env.AWS_REGION,
+// });
+
+// const s3 = new AWS.S3();
+// export default s3;
+
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
+
+export default s3;
